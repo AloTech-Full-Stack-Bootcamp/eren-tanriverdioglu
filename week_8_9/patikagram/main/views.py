4283be89b8fdd6d657024ab82eee17a006f19f39
eren <erentnr@yandex.com>
2021-12-18 19:51:39 +0300

    Add permission

--- week_8_9/patikagram/main/views.py
+++ week_8_9/patikagram/main/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import UserSerializer, PostSerializer, CommentSerializer, LikeSerializer
 from .models import Post, Like, Comment
@@ -9,18 +9,22 @@ from .models import Post, Like, Comment
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class LikeViewSet(viewsets.ModelViewSet):
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
